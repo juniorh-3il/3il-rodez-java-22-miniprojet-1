bf3b75192f1e3c2bf39da156ef980316834d9162
juniorh-3il <juniorh@3il.fr>
2023-12-14 11:57:35 +0100

    🚑️ Correction de la classe BruitAleatoire: complétion du constructeur, (re)définition de la méthode bruit2D (cf. 'Faites du bruit, mais aléatoire')

--- src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
+++ src/fr/ecole3il/rodez2023/perlin/math/BruitAleatoire.java
@@ -8,6 +8,24 @@ import java.util.Random;
  */
 public class BruitAleatoire extends Bruit2D {
 
-    public BruitAleatoire(long graine, double resolution);
-    
+    /**
+     * Constructeur de la classe BruitAleatoire.
+     * @param graine La graine utilisée pour initialiser le générateur de bruit.
+     * @param resolution La resolution utilisée pour paramétrer la granularité de l'image générée.
+     */
+    public BruitAleatoire(long graine, double resolution) {
+        super(graine, resolution);
+    }
+
+    /**
+     * Génère un double aléatoire.
+     * @param x Coordonnée x pour laquelle obtenir le bruit.
+     * @param y Coordonnée y pour laquelle obtenir le bruit.
+     * @return un double aléatoire.
+     */
+    @Override
+    public double bruit2D(double x, double y) {
+        Random r = new Random(this.getGraine());
+        return r.nextDouble();
+    }
 }
